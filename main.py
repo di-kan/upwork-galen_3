393299fbe11c0e047d60458e5117ae024e47e400
Dimitris(w11) <59868746+2bmdt@users.noreply.github.com>
2023-10-05 18:51:12 +0300

    Forgot to update version to 1.0.2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     logging.basicConfig(filename='crawler.log', level=logging.INFO, filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
     logging.info(f"Starting app...")
     eng = Scraper()
-    version = "1.0.1"
+    version = "1.0.2"
     default_title = f"Dissolved/Active - Georgia State v{version}"
     app = wx.App(False)
     frame = MainWindow(None, default_title, eng)
